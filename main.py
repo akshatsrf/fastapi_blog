740affd0abbe38fb8b984de9807fdc729ee7f5f9
akshatsrf <akshatsrf860@gmail.com>
2022-05-12 18:38:47 +0530

    added delete blog functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,status, Response
 import schemas,models
 from sqlalchemy.orm import Session
 from database import engine,SessionLocal
@@ -19,7 +19,7 @@ def get_db():
 def func():
     return "Add /docs in the URL for Swagger UI"
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(blog: schemas.Blog, db: Session = Depends(get_db)):
     new_blog  = models.Blog(title = blog.title, body = blog.body)
     db.add(new_blog)
@@ -32,7 +32,17 @@ def all( db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def specific(id: int, db: Session = Depends(get_db)):
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'blog deleted'
+
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def specific(id: int,response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'detail' : f'Blog with the id {id} is not available'}
     return blog
