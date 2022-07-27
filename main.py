0abc4d459ee8f3dd2cbe787f679940b6e1f8451e
akshatsrf <akshatsrf860@gmail.com>
2022-07-27 18:25:09 +0530

    added update

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not found')
-    blog.update(request)
+    blog.update(dict(request))
     db.commit()
     return 'updated sucessfully'
 
